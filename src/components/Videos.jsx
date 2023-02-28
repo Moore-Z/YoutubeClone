6a1e8e52c9d03b89bddffd7d9a3ed62d1e0d0eda
HenryZhu <zhumoore9661996@gmail.com>
2023-02-28 13:14:34 -0500

    new Files

---
+++ src/components/Videos.jsx
@@ -0,0 +1,20 @@
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard, Loader } from './'
+const Videos = ({ videos }) => {
+  if (!videos?.length) return <Loader />
+
+
+  return (
+    <Stack direction="row" flexWrap="wrap" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+
+        <Box key={idx} >
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack >
+  )
+}
+export default Videos
+
